297c1a04981386baea93fbeb0d7ebe513f2b3c59
roxzlir <emil.ch.nordin@gmail.com>
2023-11-10 11:03:14 +0100

    Added Interface

---
+++ ICheckOut.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubBranchDemo
+{
+    internal interface ICheckOut
+    {
+        public void CheckOut();
+    }
+}
